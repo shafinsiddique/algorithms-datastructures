563b1fac4b7bf42257b572966fdc6d677bfbad63
shafinsiddique <siddiqueshafin20@gmail.com>
2019-06-27 01:25:45 -0400

    - completed reshapeMatrix

--- leetcode/arrays.py
+++ leetcode/arrays.py
@@ -19,8 +19,80 @@ def flipandInvertImage(a):
             else:
                 a[x][y] = 0
 
-l = [[1,1,0],[1,0,1],[0,0,0]]
-flipandInvertImage(l)
-print(l)
+def sortArrayByParity(l):
+    sortedbyparity = []
+    for items in l:
+        if items % 2 == 0:
+            sortedbyparity.append(items)
+
+    for items in l:
+        if items % 2 != 0:
+            sortedbyparity.append(items)
+
+    return sortedbyparity
+
+def heightChecker(A):
+    sortlist = sorted(A)
+    counter = 0
+    for x in range(len(A)):
+        if A[x] != sortlist[x]:
+            counter += 1
+
+    return counter
+
+
+def shapeMatric(l, r, c):
+    shaped = []
+    counter = 0
+    cr = 0
+    cl = 0
+    for x in range(r):
+        row = []
+
+        while len(row) < c:
+            row.append(l[cr][cl])
+
+            if cl == len(l[cr])-1:
+                cr += 1
+                cl = 0
+
+            else:
+                cl += 1
+
+        shaped.append(row)
+
+    return shaped
+
+
+
+
+
+def reshapeMatrix(l, r, c):
+    """Reshape the matrix."""
+
+    rows = len(l)
+    cols = len(l[0])
+
+    # Dimensions are Rows * Cols.
+    # NEed to reshape to r * c.
+
+    if rows * cols == r * c:
+        l = shapeMatric(l, r, c)
+
+    return l
+
+def singleNumber(l):
+    temp = []
+    for items in l:
+        if items in temp:
+            temp.remove(items)
+
+        else:
+            temp.append(items)
+
+    return temp[0]
+
+
+print(reshapeMatrix([[1,2],[3,4]],1,4))
 
 
