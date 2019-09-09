e753a55b297d238f783d4c789b9218b278e69b24
shafinsiddique <siddiqueshafin20@gmail.com>
2019-09-09 03:01:24 -0400

    - started epi.

---
+++ epi/arrays.py
@@ -0,0 +1,23 @@
+def partition(nums: list):
+    """Partition the array.
+
+    O(n) time and O(N) space.
+    """
+
+
+    smaller = []
+    equal = []
+    larger =[]
+    pivot = nums[0]
+    for items in nums:
+        if items < pivot:
+            smaller.append(items)
+
+        elif items == pivot:
+            equal.append(items)
+
+        else:
+            larger.append(items)
+
+    return smaller + equal + larger
+
