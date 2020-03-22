a268e366aae7135a31862d46c5bd470915c5c121
shafinsiddique <siddiqueshafin20@gmail.com>
2020-03-22 08:29:54 -0400

    changed c# file naming.

--- algorithms/UnionFind.cs
+++ algorithms/UnionFind.cs
@@ -2,14 +2,32 @@
 
 namespace algorithms
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            InefficientUnionFind h = new InefficientUnionFind(4);
-            h.union(0,3);
-            h.union(2,3);
-            System.Console.WriteLine(h.connected(1,2));
-        }
-    }
+   class InefficientUnionFind {
+       int[]ids;
+       public InefficientUnionFind(int n) {
+           this.ids = new int[n];
+
+           for (int x=0; x<n;x++) {
+               this.ids[x] = x;
+           }
+       }
+
+       public Boolean connected(int p1, int p2){
+           return this.ids[p1] == this.ids[p2];
+       }
+
+       public void union(int p1, int p2){
+           int id1 = this.ids[p1];
+           int id2 = this.ids[p2];
+
+           for (int x=0; x<this.ids.Length; x++) {
+               if (this.ids[x] == id1) {
+                   this.ids[x] = id2;
+               }
+           }
+
+       }
+
+       
+   }
 }
