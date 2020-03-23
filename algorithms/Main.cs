36231838af4696c59b46fed3f5c96e877d4f0f32
shafinsiddique <siddiqueshafin20@gmail.com>
2020-03-23 07:28:25 -0400

    implemented quick union algo

--- algorithms/Main.cs
+++ algorithms/Main.cs
@@ -1,15 +1,14 @@
 ﻿using System;
-
 namespace algorithms
 {
     class Program
     {
         static void Main(string[] args)
         {
-            InefficientUnionFind h = new InefficientUnionFind(4);
+            UnionFind h = new QuickUnion(4);
             h.union(0,3);
             h.union(2,3);
-            System.Console.WriteLine(h.connected(1,2));
+            System.Console.WriteLine(h.connected(0,2));
         }
     }
 }
--- algorithms/UnionFind.cs
+++ algorithms/UnionFind.cs
@@ -2,9 +2,13 @@
 
 namespace algorithms
 {
-   class InefficientUnionFind {
+    interface UnionFind {
+        public Boolean connected(int p1, int p2);
+        public void union(int p1, int p2);
+     }
+   class QuickFind : UnionFind {
        int[]ids;
-       public InefficientUnionFind(int n) {
+       public QuickFind(int n) {
            this.ids = new int[n];
 
            for (int x=0; x<n;x++) {
@@ -25,9 +29,34 @@ namespace algorithms
                    this.ids[x] = id2;
                }
            }
+       }
+   }
+   class QuickUnion : UnionFind{
+       int[]ids;
+
+       public QuickUnion(int n) {
+           this.ids = new int[n];
+           for (int x=0; x<this.ids.Length; x++) {
+               this.ids[x] = x;
+           }
+       }
+       public Boolean connected(int p1, int p2) {
+           return root(p1) == root(p2);
+       }
+
+       public void union(int p1, int p2) {
+            this.ids[root(p1)] = root(p2);
+       }
+
+       private int root(int p) {
+           if (ids[p] == p){ 
+               return p;
+           }
+           return root(ids[p]);
 
        }
 
-       
+
+    
    }
 }
