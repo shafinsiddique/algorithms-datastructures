5c41b3ee40a7eb5674e87317ea71f8e66f6b3ff8
shafinsiddique <siddiqueshafin20@gmail.com>
2020-04-24 16:29:24 -0400

    started wordnet graphs assignment.

---
+++ algorithms/graphs-coursera-assignment/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace graphs_coursera_assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WordNet wn = new WordNet("synsets.txt","hypernym.txt");
+
+            foreach (String noun in wn.getNouns()) {
+                System.Console.WriteLine(noun);
+            }
+
+        }
+    }
+}
---
+++ algorithms/graphs-coursera-assignment/Wordnet.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+public class WordNet
+{
+    HashSet<String> nouns = new HashSet<string>();
+    public WordNet(String synsets, String hypernyms)
+    {
+        addAllNouns(synsets);
+
+
+    }
+
+    private void addAllNouns(String synsets)
+    {
+        char[] lineseperators = new char[] { ',' };
+        char[] nounseperators = new char[] {' '};
+        using (StreamReader synsetsFile = File.OpenText(synsets))
+        {
+            String line;
+
+            while ((line = synsetsFile.ReadLine()) != null)
+            {
+                String nouns = line.Split(lineseperators, StringSplitOptions.None)[1];
+                String[] nounsSplit = nouns.Split(nounseperators, StringSplitOptions.None);
+
+                for (int x=0; x<nounsSplit.Length; x++) {
+                    this.nouns.Add(nounsSplit[x]);
+                }
+                
+            }
+        }
+    }
+
+    public HashSet<String> getNouns() {
+        return nouns;
+    }
+
+}
