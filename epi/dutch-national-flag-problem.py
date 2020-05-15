b3a2b42d3f0d5fd00f6340cb152bb78ac1adaa4a
shafinsiddique <siddiqueshafin20@gmail.com>
2020-05-15 00:49:27 -0400

    DNF Problem From EPI.

--- epi/dutch-national-flag-problem.py
+++ epi/dutch-national-flag-problem.py
@@ -40,6 +40,7 @@ def partitioning(arr, pivot):
 
     return smaller + equal + bigger
 
+
 def partioning_constant_space(arr, pivot):
     """This approach takes o(1) space but o(n^2) time. Definitely not the most optimal solution
     but definitely interesting to know."""
@@ -57,10 +58,54 @@ def partioning_constant_space(arr, pivot):
         for y in range(x):
             if arr[y] > pivot:
                 arr[x],arr[y] = arr[y], arr[x]
+                break
+
+def dutch_national_flag_constant_space(arr, pivot):
+    """same as above."""
+
+    # first pass, bring all elements smaller than pivot to the front.
+    for x in range(len(arr)):
+        for y in range(x+1, len(arr)):
+            if arr[y] < pivot:
+                arr[x],arr[y] = arr[y],arr[x]
+                break
+    for x in range(len(arr)-1,-1,-1):
+        if arr[x] < pivot:
+            break # end because last element smaller than pivot has been found
+
+        # else, this element is not smaller than pivot, so we wanna see if we can find an
+        # element bigger than pivot to replace this with.
+
+        for y in range(x):
+            if arr[y] > pivot:
+                arr[y],arr[x] = arr[x], arr[y]
 
 
-l = [1,2,4,9,6,8,2,1]
 
-partioning_constant_space(l, 8)
+def dutch_national_flag_problem(arr, pivot):
+    """optimal solution for the dutch national flag problem."""
+
+    smaller = 0
+    # pass 1 : group all smaller elements together.
+    for x in range(len(arr)):
+        if arr[x] < pivot:
+            arr[x], arr[smaller] = arr[smaller], arr[x]
+            smaller += 1
+
+
+    # pass 2:
+    larger = len(arr)-1
+
+    for x in range(len(arr)-1,-1,-1):
+        if arr[x] < pivot:
+            break # found last element that is msaller than pivot. all elements
+            # before it
+
+        elif arr[x] > pivot:
+            arr[x], arr[larger] = arr[larger],arr[x]
+            larger -=1
+
+l = [1, 2, 4, 9, 6, 8, 2, 1]
+dutch_national_flag_problem(l, 8)
 print(l)
 
